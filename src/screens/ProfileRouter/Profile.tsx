500d90e237a1aca4bfa2571db9ac8ade7e505b04
Rikelme Griep <grieprikelme@gmail.com>
2023-03-28 20:13:45 -0300

    start payments button

--- src/screens/ProfileRouter/Profile.tsx
+++ src/screens/ProfileRouter/Profile.tsx
@@ -15,104 +15,114 @@ const Profile = ({
   const { user, logout } = useAuth();
   const { folders } = useFolders();
 
+  if (!user) return <Loading />;
+
   return (
     <ScreenContainer withScroll>
       <Title>Your profile</Title>
-      {user ? (
-        <View style={{ marginTop: 24, alignItems: "center" }}>
-          <Image
-            style={{ width: 120, height: 120, borderRadius: 60 }}
-            source={{ uri: user.imageUrl }}
-          />
-          <Text
-            style={{
-              color: theme.colors.olive.DEFAULT,
-              fontSize: 20,
-              fontWeight: "500",
-              marginTop: 16,
-            }}
-          >
-            {user.name}
+      <View style={{ marginTop: 24, alignItems: "center" }}>
+        <Image
+          style={{ width: 120, height: 120, borderRadius: 60 }}
+          source={{ uri: user.imageUrl }}
+        />
+        <Text
+          style={{
+            color: theme.colors.olive.DEFAULT,
+            fontSize: 20,
+            fontWeight: "500",
+            marginTop: 16,
+          }}
+        >
+          {user.name}
+        </Text>
+        <Text
+          style={{
+            marginTop: 4,
+            color: theme.colors.olive.DEFAULT,
+            fontSize: 16,
+            marginBottom: 16,
+          }}
+        >
+          {user.slogan}
+        </Text>
+        <Button
+          colorScheme="olive"
+          onPress={() => navigation.navigate("ChangePassword")}
+        >
+          Change your password
+        </Button>
+        <View
+          style={{
+            borderBottomWidth: 1,
+            borderBottomColor: theme.colors.floral.dark,
+            alignSelf: "stretch",
+            margin: 24,
+          }}
+        />
+        <Text style={{ fontSize: 18, fontWeight: "600" }}>
+          <Text style={{ color: theme.colors.flame.DEFAULT }}>
+            {user.plan === "free" ? "Free" : "Premium"}
+          </Text>{" "}
+          plan
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 8 }}>
+          <Text style={{ color: theme.colors.flame.DEFAULT }}>
+            {folders?.length}
+          </Text>{" "}
+          folder(s)
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 8 }}>
+          <Text style={{ color: theme.colors.flame.DEFAULT }}>
+            {folders?.reduce(
+              (acc, folder) => acc + (folder.counts.text || 0),
+              0
+            )}
+          </Text>{" "}
+          text(s)
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 8 }}>
+          <Text style={{ color: theme.colors.flame.DEFAULT }}>
+            {folders?.reduce(
+              (acc, folder) => acc + (folder.counts.file || 0),
+              0
+            )}
+          </Text>{" "}
+          file(s)
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 8 }}>
+          Securing things since{" "}
+          <Text style={{ color: theme.colors.flame.DEFAULT }}>
+            {new Date(user.createdAt).toLocaleDateString()}
           </Text>
-          <Text
-            style={{
-              marginTop: 4,
-              color: theme.colors.olive.DEFAULT,
-              fontSize: 16,
-              marginBottom: 16,
-            }}
-          >
-            {user.slogan}
-          </Text>
+        </Text>
+
+        {user.plan === "premium" && (
           <Button
-            colorScheme="olive"
-            onPress={() => navigation.navigate("ChangePassword")}
-          >
-            Change your password
-          </Button>
-          <View
             style={{
-              borderBottomWidth: 1,
-              borderBottomColor: theme.colors.floral.dark,
-              alignSelf: "stretch",
-              margin: 24,
+              marginTop: 24,
             }}
-          />
-          <Text style={{ fontSize: 18, fontWeight: "600" }}>
-            <Text style={{ color: theme.colors.flame.DEFAULT }}>
-              {user.plan === "free" ? "Free" : "Premium"}
-            </Text>{" "}
-            plan
-          </Text>
-          <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 8 }}>
-            <Text style={{ color: theme.colors.flame.DEFAULT }}>
-              {folders?.length}
-            </Text>{" "}
-            folder(s)
-          </Text>
-          <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 8 }}>
-            <Text style={{ color: theme.colors.flame.DEFAULT }}>
-              {folders?.reduce(
-                (acc, folder) => acc + (folder.counts.text || 0),
-                0
-              )}
-            </Text>{" "}
-            text(s)
-          </Text>
-          <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 8 }}>
-            <Text style={{ color: theme.colors.flame.DEFAULT }}>
-              {folders?.reduce(
-                (acc, folder) => acc + (folder.counts.file || 0),
-                0
-              )}
-            </Text>{" "}
-            file(s)
-          </Text>
-          <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 8 }}>
-            Securing things since{" "}
-            <Text style={{ color: theme.colors.flame.DEFAULT }}>
-              {new Date(user.createdAt).toLocaleDateString()}
-            </Text>
-          </Text>
-          <View
-            style={{
-              borderBottomWidth: 1,
-              borderBottomColor: theme.colors.floral.dark,
-              alignSelf: "stretch",
-              margin: 24,
-            }}
-          />
-          <Button
-            style={{ paddingHorizontal: 48 }}
             colorScheme="olive"
-            onPress={logout}
           >
-            Log out
+            Access payments
           </Button>
-        </View>
-      ) : (
-        <Loading />
-      )}
+        )}
+
+        <View
+          style={{
+            borderBottomWidth: 1,
+            borderBottomColor: theme.colors.floral.dark,
+            alignSelf: "stretch",
+            margin: 24,
+          }}
+        />
+        <Button
+          style={{ paddingHorizontal: 48 }}
+          colorScheme="olive"
+          onPress={logout}
+        >
+          Log out
+        </Button>
+      </View>
     </ScreenContainer>
   );
 };
